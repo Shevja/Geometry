f8050c80aa56349d6dcb4bb34fa45ff2880a00bd
Каширин Владислав Сергеевич <iv822s08@bwp15.csc.local>
2019-05-04 13:00:05 +0700

    add main.c add function area

--- main.c
+++ main.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
+#define PI 3.14
+
+void area(float r1)
+{
+    float s = PI * r1;
+    printf("Площадь фигуры равна %f\n", &s);
+}
 
 int main()
 {
-    int n, m, i;
-    int *a, *b;
+    float x1, x2, y1, y2, r1, r2; // s1, s2;
+
+    printf("Введите координаты (x и y) центра первой окружности: \n");
+    scanf("%f %f", &x1, &y1);
+    printf("Введите радиус первой окружности: \n");
+    scanf("%f", &r1);
+    printf("Введите координаты (x и y) центра второй окружности: \n");
+    scanf("%f%f", &x2, &y2);
+    printf("Введите радиус второй окружности: \n");
+    scanf("%f", &r2);
     
-    printf("Колво вершин первой фигуры: ");
-    scanf("%d\n", &n);
-    getchar();
-    printf("Колво вершин второй фигуры: ");
-    scanf("%d\n", &m);
-    getchar();
+    if ((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1) > (r1+r2)*(r1+r2))
+	printf("Фигуры не пересекаются\n");
+    else 
+	printf("Фигуры пересекаются\n");
+
+    area(r1);
+    
+
+
+
+/*    printf("Колво вершин первой фигуры:\n ");
+    scanf("%d", &n);
+    printf("Колво вершин второй фигуры:\n ");
+    scanf("%d", &m);
     
     a = (int*)malloc(n * sizeof(int));
     b = (int*)malloc(m * sizeof(int));
@@ -23,7 +46,6 @@ int main()
 	scanf("%d", &a[i]);
 	printf("\tpoint %d = ", i);
 	printf("%d\n", a[i]);
-	getchar();
     }
 
     printf("Введите координаты второй фигуры\n");
@@ -32,8 +54,9 @@ int main()
 	scanf("%d", &b[i]);
 	printf("\tpoint %d = ", i);
 	printf("%d\n", b[i]);
-	getchar();
-    }
+    }*/
+    
+    
 
     return 0;
 }
