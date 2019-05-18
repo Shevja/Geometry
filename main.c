43fe3204f4292a56b7e8db6eb66c9521c60c08a6
Каширин Владислав Сергеевич <iv822s08@bwp15.csc.local>
2019-05-18 12:24:22 +0700

    add travis file

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <malloc.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 void area(float r1, float pi)
 {
     float s = pi * r1;
-    //float *p;
-    //p = &s;
+    // float *p;
+    // p = &s;
     printf("%f\n\n", s);
 }
 
@@ -28,12 +28,12 @@ int main()
 
     printf("\nPoint_1 = %f\tPoint_2 = %f\n\tRadius_2 = %f\n\n", x2, y2, r2);
 
-    if ((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1) > (r1+r2)*(r1+r2))
-	printf("Фигуры не пересекаются\n");
-    else 
-	printf("Фигуры пересекаются\n");
+    if ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) > (r1 + r2) * (r1 + r2))
+        printf("Фигуры не пересекаются\n");
+    else
+        printf("Фигуры пересекаются\n");
 
     area(r1, pi);
-    
+
     return 0;
 }
